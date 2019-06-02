6b9627d603e00de00ef32632088f4615529b3812
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2019-06-02 01:11:00 +0000

    test heroku

--- src/main/java/com/senebien/config/HibernateInitializerConfig.java
+++ src/main/java/com/senebien/config/HibernateInitializerConfig.java
@@ -17,7 +17,7 @@ public class HibernateInitializerConfig {
     static {
         try {
             Configuration configuration = new Configuration();
-            configuration.configure("/hibernate.cfg.xml");
+            configuration.configure("/hibernate.cfg-heroku.xml");
 
             ourSessionFactory = configuration.buildSessionFactory();
         } catch (Throwable ex) {
