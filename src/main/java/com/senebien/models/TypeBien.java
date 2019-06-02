0f4f4b3f5f34d7b8e031139ff3f4be344f81028c
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2019-06-02 23:01:15 +0000

    add entities

---
+++ src/main/java/com/senebien/models/TypeBien.java
@@ -0,0 +1,15 @@
+package main.java.com.senebien.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class TypeBien implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String libelle;
+}
